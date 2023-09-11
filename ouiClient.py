f3844cc32958893f226242846a4270f4006b02b0
Grégory MOU KUI <gmoukui@piwwop.com>
2023-09-11 16:26:40 +0200

    Added Logger

--- ouiClient.py
+++ ouiClient.py
@@ -1,10 +1,17 @@
 import os
+import logging
 from dotenv import load_dotenv
 
 import discord
 
 load_dotenv()
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 ouiClient = discord.Bot()
+
+@ouiClient.event
+async def on_ready():
+    logging.info('Logged in as ' + ouiClient.user.name)
 
 ouiClient.run(os.getenv("OUI_DISCORD_TOKEN"))
