111446b5158d710cdd5664369ee96a59b0b8290d
AyerMan <firzamdigidix@gmail.com>
2024-01-11 11:06:10 +0100

    refacto using last discord.py

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ import logging
 from dotenv import load_dotenv
 
 import discord
+from discord.ext import tasks
 
 load_dotenv()
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 
-wattsyClient = discord.Bot()
+intents = discord.Intents.default()
+intents.message_content = True
 
-@tasks.loop(hours=1)
-async def capture_pokemon():
-    channel_to_capture_pokemon = wattsyClient.get_channel("995709716598624367")
-    wattsyClient.send("$p")
+wattsyClient = discord.Client(intents=intents)
 
 @wattsyClient.event
 async def on_ready():
     logging.info('Logged in as ' + wattsyClient.user.name)
 
-capture_pokemon.start()
+
 wattsyClient.run(os.getenv("OUI_DISCORD_TOKEN"))
