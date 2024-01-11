68cb34fe3b98bbff4230fed8a9e422630a565e99
AyerMan <firzamdigidix@gmail.com>
2024-01-11 09:52:16 +0100

    Rename + add capture scheduler

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 wattsyClient = discord.Bot()
 
+@tasks.loop(hours=1)
+async def capture_pokemon():
+    channel_to_capture_pokemon = wattsyClient.get_channel("995709716598624367")
+    wattsyClient.send("$p")
+
 @wattsyClient.event
 async def on_ready():
     logging.info('Logged in as ' + wattsyClient.user.name)
 
+capture_pokemon.start()
 wattsyClient.run(os.getenv("OUI_DISCORD_TOKEN"))
